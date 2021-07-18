c7b482db850d9c66af55186b0c04fcc6820a648b
azimK-shit <dogkone01@gmail.com>
2021-07-18 01:01:58 +0000

    JELMPB

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //déclencher une action sans addEventListener mais avec onclick Event
 const leBouton = document.querySelector("#commander")
 leBouton.onclick = () => {
-    alert(" Vous serrez orienter vers un Appel payant (Tarif Comminucation Usuel")
+    alert("Vous serrez orienter vers un Appel payant (Tarif Comminucation Usuel)")
 }
 
 console.log(leBouton);
