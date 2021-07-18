20690b9cd24a78b2ce32ea07942c5d5f08d665f8
azimK-shit <dogkone01@gmail.com>
2021-07-18 22:44:01 +0000

    jelmpb

--- js/entrer en conctact.js
+++ js/entrer en conctact.js
@@ -2,7 +2,7 @@ let btnEnvoyer = document.querySelector("button");
 console.log(btnEnvoyer);
 
 btnEnvoyer.addEventListener("click", () => {
-
-
-
-})
+    //récupérer les données du formulaire
+    let genre = document.querySelectorAll("input [name = 'genre']");
+    console.log(genre);
+});
